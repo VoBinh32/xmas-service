ae6a5536f3e273c0b99e7399ac4f0b4dee0ecac9
Thanh Bình Võ <thanhbinh322000@gmail.com>
2021-04-18 23:45:54 +0300

    modify sort

--- src/selectors/sort.ts
+++ src/selectors/sort.ts
@@ -1,10 +1,5 @@
 import { ICart, IProduct } from "../types";
 
-export const sortByAmount = (products: IProduct[]) => {
-  return products.sort((a: IProduct, b: IProduct) => {
-    return a.price < b.price ? 1 : -1;
-  });
-};
 export const sortByApproved = (carts: ICart[]) =>
   carts.map((cart: ICart) => {
     return cart.products.filter(
